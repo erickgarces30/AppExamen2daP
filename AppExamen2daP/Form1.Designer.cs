25b8047f1eefc6a14bb56552636c7534dee55597
Erick Garces <Erick Garces@ERICK>
2019-12-04 11:27:20 -0500

    Agregar archivos de proyecto.

---
+++ AppExamen2daP/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace AppExamen2daP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtIngreso = new System.Windows.Forms.TextBox();
+            this.txtSuma = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingresa un número:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Suma de primos:";
+            // 
+            // txtIngreso
+            // 
+            this.txtIngreso.Location = new System.Drawing.Point(178, 34);
+            this.txtIngreso.Name = "txtIngreso";
+            this.txtIngreso.Size = new System.Drawing.Size(100, 20);
+            this.txtIngreso.TabIndex = 2;
+            this.txtIngreso.TextChanged += new System.EventHandler(this.txtIngreso_TextChanged);
+            // 
+            // txtSuma
+            // 
+            this.txtSuma.Location = new System.Drawing.Point(178, 89);
+            this.txtSuma.Name = "txtSuma";
+            this.txtSuma.ReadOnly = true;
+            this.txtSuma.Size = new System.Drawing.Size(100, 20);
+            this.txtSuma.TabIndex = 3;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(89, 151);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(188, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 215);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtSuma);
+            this.Controls.Add(this.txtIngreso);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Sumatoria";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtIngreso;
+        private System.Windows.Forms.TextBox txtSuma;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ AppExamen2daP/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppExamen2daP
+{
+    public partial class Form1 : Form
+    {
+        //
+        private long esPrimo(int n)
+        {
+            int num = 0, cont=0;
+            for(int a=2;a<=n;a++)
+            {
+                bool primo = true;
+                int b = 2;
+                    while(primo==true && b<=(a/2))
+                {
+                    if(a%b==0)
+                    {
+                        primo = false;
+                    }
+                    b++;
+                }
+                if (primo == true)
+                {
+                    cont = 1 + cont;
+                    num = num + a;
+                }
+            }
+            return num;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int n;
+            n = int.Parse(this.txtIngreso.Text);
+            long primo = esPrimo(n);
+            this.txtSuma.Text = primo.ToString();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtIngreso_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
